0fdac4eb9a66cbf6a9368d8776a064a3637161d0
11000783 <josephaguilar2022@gmail.com>
2018-10-27 17:28:19 -0700

    I did Dice Roll, Rock Paper scissers, and intro to random still in proggress.

--- src/random/IntroToRandom.java
+++ src/random/IntroToRandom.java
@@ -13,12 +13,13 @@ public class IntroToRandom {
 		//3. Now make r random. 
 		//   Create an object of the Random class
 		//	 use .nextInt() to get a random number
-		
-
+		Random Bob=new Random();
+;
 		//4. Limit the random number between 0 and 100
-
+		int randomChoice=Bob.nextInt(101);
 		//5. Limit the random number between 25 and 75. Hint: ((highest value - lowest value)+1) + lowest value
-
+		int randomChoice=Bob.nextInt(26);
+		int randomChoice=Bob.nextInt(76);
 		//6. Challenge: Limit the random number between -222 and 88
 
 		//1. Print out the value of r
