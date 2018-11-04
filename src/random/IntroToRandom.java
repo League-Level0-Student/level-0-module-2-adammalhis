51338e1b62f864f63cd7e37627d5aa0333e887a7
adammalhis <league@wts22.attlocal.net>
2018-11-04 12:57:18 -0800

    I did Magic 8 Ball and Intro to random still in proggress!!!!!!

--- src/random/IntroToRandom.java
+++ src/random/IntroToRandom.java
@@ -6,25 +6,32 @@ package random;
 import java.util.Random;
 
 public class IntroToRandom {
-	public static void main(String[] args) {
-		
-		int r = 0;
-		
-		//3. Now make r random. 
-		//   Create an object of the Random class
-		//	 use .nextInt() to get a random number
-		Random Bob=new Random();
-;
-		//4. Limit the random number between 0 and 100
-		int randomChoice=Bob.nextInt(101);
-		//5. Limit the random number between 25 and 75. Hint: ((highest value - lowest value)+1) + lowest value
-		int randomChoice=Bob.nextInt(26);
-		int randomChoice=Bob.nextInt(76);
-		//6. Challenge: Limit the random number between -222 and 88
+	public static void main(String[] nargs) {
 
-		//1. Print out the value of r
-		
-		//2. Run the program.  What number appears in the console?
-		//   Run it again. Is the number the same?
+		int r = 0;
+
+		// 3. Now make r random.
+		// Create an object of the Random class
+		// use .nextInt() to get a random number
+		Random Bob = new Random();
+		;
+		// 4. Limit the random number between 0 and 100
+		int randomChoice = Bob.nextInt(101);
+		// 5. Limit the random number between 25 and 75. Hint: ((highest value - lowest
+		// value)+1) + lowest value
+		int randomChoice2 = Bob.nextInt(((88 - -222) + 1) + -222);
+
+		System.out.println(randomChoice2);
+
+		// 6. Challenge: Limit the random number between -222 and 88
+		int randomChoice3 = Bob.nextInt(((88 - -222) + 1) + -222);
+		System.out.println(randomChoice3);
+
+		int randomChoice22 = Bob.nextInt(((88 - -222) + 1) + -222);
+
+		// 1. Print out the value of r
+
+		// 2. Run the program. What number appears in the console?
+		// Run it again. Is the number the same?
 	}
 }
--- src/random/Magic8Ball.java
+++ src/random/Magic8Ball.java
@@ -1,35 +1,52 @@
 //    Copyright (c) The League of Amazing Programmers 2013-2018
 //    Level 0
 
-
 package random;
 
 import java.util.Random;
 
+import javax.swing.JOptionPane;
+
 public class Magic8Ball {
 
 	// 1. Make a main method that includes all the steps below….
+	public static void main(String[] args) {
 
-	// 2. Make a variable that will hold a random number and put a random number into this variable using "new Random().nextInt(4)"
+		// 2. Make a variable that will hold a random number and put a random number
+		// into this variable using "new Random().nextInt(4)"
+		Random Bob = new Random();
+		int number = Bob.nextInt(4);
+		// 3. Print out this variable
+		System.out.println(number);
+		// TODO Auto-generated method stub
 
-	// 3. Print out this variable
+		// TODO Auto-generated method stub
 
-	// 4. Get the user to enter a question for the 8 ball
+		// 4. Get the user to enter a question for the 8 ball
+		JOptionPane.showInputDialog(null, "I am a Magic 8 ball give me a question");
+		// 5. If the random number is 0
 
-	// 5. If the random number is 0
+		// -- tell the user "Yes"
+		if (number == 0) {
+			JOptionPane.showMessageDialog(null, "Yes");
+		}
+		// 6. If the random number is 1
+		if (number == 1) {
+			JOptionPane.showMessageDialog(null, "NO");
+		}
+		// -- tell the user "No"
 
-	// -- tell the user "Yes"
+		// 7. If the random number is 2
+		if (number == 2) {
+			JOptionPane.showMessageDialog(null, "Maybe you should ask google");
+		}
+		// -- tell the user "Maybe you should ask Google?"
 
-	// 6. If the random number is 1
+		// 8. If the random number is 3
+		if (number == 3) {
+			JOptionPane.showMessageDialog(null, "NO");
+		}
+		// -- write your own answer
 
-	// -- tell the user "No"
-
-	// 7. If the random number is 2
-
-	// -- tell the user "Maybe you should ask Google?"
-
-	// 8. If the random number is 3
-
-	// -- write your own answer
-
+	}
 }
